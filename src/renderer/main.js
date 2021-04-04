78f2ca3721f8352b549c762eca1c4b4053e2944e
Tim <tymmesyde@gmail.com>
2021-04-04 20:37:20 +0200

    chore: fix project structure

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import Ionicons from './plugins/ionicons';
 import VueScrollTo from 'vue-scrollto';
 
 createApp(App)
+  .use(Ionicons)
   .use(VueScrollTo)
   .mount('#app');
