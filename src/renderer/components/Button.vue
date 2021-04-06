45a15e1f7f13beb92a829fe193c0180331deb013
Tim <tymmesyde@gmail.com>
2021-04-06 23:33:31 +0200

    feat: add clear history button

---
+++ src/renderer/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+    <button>
+        <ion-icon :name="icon"></ion-icon>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        icon: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    background: transparent;
+    border: 2px solid $text-color;
+    border-radius: 35px;
+    padding-left: 13px;
+    padding-right: 17px;
+    font-family: 'Montserrat-Bold';
+    font-size: 14px;
+    color: $text-color;
+    outline: none;
+    transition: all 0.1s ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        background-color: $text-color;
+        color: $background-color;
+    }
+}
+</style>
--- src/renderer/components/History.vue
+++ src/renderer/components/History.vue
@@ -1,31 +1,43 @@
 <template>
-    <ul class="history" ref="history">
-        <h3 @click="scrollToBottom()">
+    <div class="history" ref="history">
+        <div class="title" @click="scrollToBottom()">
             <ion-icon name="receipt-outline"></ion-icon>
             History
-        </h3>
+        </div>
 
-        <li v-for="[hostname, hits] in sortedHistory" :key="hostname">
-            <div class="hostname">
-                {{ hostname }}
-            </div>
-            <div class="hits">
-                {{ hits > 99 ? hits + '+' : hits }}
-            </div>
-        </li>
-
-        <div class="empty" v-show="isEmpty">
+        <div class="placeholder" v-if="isEmpty">
             <ion-icon name="alert-circle-outline"></ion-icon>
             No records
         </div>
-    </ul>
+
+        <div class="content" v-else>
+            <div class="list">
+                <div class="item" v-for="[hostname, hits] in sortedHistory" :key="hostname">
+                    <div class="hostname">
+                        {{ hostname }}
+                    </div>
+                    <div class="hits">
+                        {{ hits > 99 ? hits + '+' : hits }}
+                    </div>
+                </div>
+            </div>
+
+            <Button icon="trash" @click="clear()">
+                CLEAR
+            </Button>
+        </div>
+    </div>
 </template>
 
 <script>
 import { Shield } from '../services';
+import Button from './Button';
 
 export default {
     name: 'History',
+    components: {
+        Button
+    },
     data() {
         return {
             history: {},
@@ -51,6 +63,10 @@ export default {
         store() {
             clearTimeout(this.storeTimeout);
             this.storeTimeout = setTimeout(() => localStorage.setItem('history', JSON.stringify(this.history)), 1000);
+        },
+        clear() {
+            this.history = {};
+            this.store();
         }
     },
     mounted() {
@@ -69,15 +85,18 @@ export default {
 .history {
     position: relative;
     height: calc(100vh - #{$header-height});
-    padding: 0 3.5vh;
-    padding-bottom: 3vh;
+    padding: 0 20px;
+    padding-bottom: 15px;
     background-color: $accent-color;
     font-family: 'Montserrat-SemiBold';
     color: transparent;
     box-shadow: 0 0 1vh rgba(0, 0, 0, 0.1);
     overflow-x: hidden;
 
-    h3 {
+    .title {
+        width: 100%;
+        display: flex;
+        gap: 12px;
         z-index: 1;
         position: sticky;
         top: 0;
@@ -86,56 +105,63 @@ export default {
         height: $history-title-height;
         margin: 0;
         background-color: $accent-color;
+        font-size: 20px;
         color: $text-color;
         cursor: pointer;
-
-        ion-icon {
-            margin-right: 2vh;
-        }
     }
 
-    li {
-        display: flex;
-        justify-content: space-between;
-        padding: 0.5vh 0;
-        font-size: 0.75rem;
-        color: $text-color;
-
-        .hostname {
-            opacity: 0.6;
-            user-select: text;
-            transition: opacity 0.15s ease-in-out;
-
-            &:hover {
-                opacity: 0.7;
-            }
-        }
-
-        .hits {
-            display: grid;
-            place-items: center;
-            height: 3.5vh;
-            min-width: 3.5vh;
-            padding: 0 1.15vh;
-            border-radius: 1.5vh;
-            background-color: $warning-color;
-            color: $text-color;
-            font-size: 2vh;
-            user-select: none;
-        }
-    }
-
-    .empty {
+    .placeholder {
         display: flex;
         align-items: center;
         justify-content: center;
+        gap: 5px;
         height: calc(100% - #{$history-title-height});
         font-family: 'Montserrat-Regular';
         color: $text-color;
         opacity: 0.5;
+    }
 
-        ion-icon {
-            margin-right: 1vh;
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+
+        .list {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            gap: 10px;
+
+            .item {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                font-size: 12px;
+                color: $text-color;
+
+                .hostname {
+                    opacity: 0.6;
+                    user-select: text;
+                    transition: opacity 0.15s ease-in-out;
+
+                    &:hover {
+                        opacity: 0.7;
+                    }
+                }
+
+                .hits {
+                    display: grid;
+                    place-items: center;
+                    height: 20px;
+                    padding: 0 7px;
+                    border-radius: 10px;
+                    background-color: $warning-color;
+                    color: $text-color;
+                    font-size: 11px;
+                    user-select: none;
+                }
+            }
         }
     }
 }
